d9ab27a1443bdb1866d9f3e28672a47718779812
sb7297 <sbaireddy7297@gmail.com>
2021-07-06 15:21:38 -0400

    event 2, pointerleave

--- js/index.js
+++ js/index.js
@@ -3,4 +3,8 @@ for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener('pointerenter', (event) => {
         event.target.style.border = "2px solid red";
     });
+
+    navLinks[i].addEventListener('pointerleave', (event) => {
+        event.target.style.border = "";
+    });
 }
